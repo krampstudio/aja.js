dff5cd7f26a76f2df7cfccef302f9f6aac297698
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-11-26 08:12:11 +0100

    first attempt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+(function(Prism){
+    var pres = document.querySelectorAll('pre');
+    for(var i  = 0; i < pres.length; i++){
+        console.log(pres[i]);
+        Prism.highlightElement(pres[i], false);
+    }
+})(window.Prism);
