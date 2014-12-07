2e0e326715e7cc618e1450a80f5226cc156555f3
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-12-07 13:09:25 +0100

    new site layout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 (function(Prism){
     var pres = document.querySelectorAll('pre');
     for(var i  = 0; i < pres.length; i++){
-        console.log(pres[i]);
         Prism.highlightElement(pres[i], false);
     }
+
+    var navs = document.querySelectorAll('nav a');
+    
 })(window.Prism);
