24f6679505408969d904dd904a88dbce98588694
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-12-08 18:05:23 +0100

    comment out code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 (function(Prism){
-    var pres = document.querySelectorAll('pre');
-    for(var i  = 0; i < pres.length; i++){
-        Prism.highlightElement(pres[i], false);
-    }
+    'use strict';
 
-    var navs = document.querySelectorAll('nav a');
-    
+    //highlight code
+    var pres;
+    if(document.querySelectorAll){
+        pres = document.querySelectorAll('pre');
+        for(var i  = 0; i < pres.length; i++){
+            Prism.highlightElement(pres[i], false);
+        }
+    }
 })(window.Prism);
