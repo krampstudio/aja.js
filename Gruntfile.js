078acebc5eedfad15b06d46e4ea1a5a5ac7a3286
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2014-12-30 16:26:40 +0100

    set up minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
                 options: {
                     port: 4040,
                     base: '.',
-                    livereload : 35729
+                    livereload: 35729
                 }
             }
         },
@@ -23,21 +23,52 @@ module.exports = function(grunt) {
             }
         },
 
+        cssmin: {
+            dist: {
+                files: {
+                    'css/style.min.css': [
+                        'css/font.css', 
+                        'css/normalize.css', 
+                        'css/layout.css', 
+                        'css/theme.css',
+                        'js/vendor/prism/themes/prism-okaidia.css',
+                        'js/vendor/prism/plugins/line-numbers/prism-line-numbers.css'
+                    ]
+                }
+            }
+        },
+
+        concat : {
+            dist: {
+                files: {
+                   'js/script.min.js' : [
+                       'js/vendor/prism/prism.js',
+                       'js/vendor/prism/plugins/line-numbers/prism-line-numbers.min.js',
+                       'js/main.js'
+                    ]
+                }
+            }
+        },
+
         watch: {
             options: {
                 livereload: 35729,
                 debounceDelay: 200
             },
             preview: {
-                files : ['index.html', 'css/*.css']
+                files: ['index.html', 'css/*.css', '!css/*.min.css'],
+                tasks : ['cssmin', 'concat']
             }
         },
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-connect'); 
-    grunt.loadNpmTasks('grunt-contrib-watch'); 
-    grunt.loadNpmTasks('grunt-open'); 
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-open');
     grunt.registerTask('preview', ['connect:preview', 'open:preview', 'watch:preview']);
+    grunt.registerTask('build', ['cssmin', 'concat']);
 
 };
